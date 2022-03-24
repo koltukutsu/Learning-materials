8d28d03d964cd8cda82d48ab70f38de73d18744e
koltukustu-zbook <semih.babacan@std.yildiz.edu.tr>
2022-03-24 14:59:01 +0300

    clean folder was created to implement code splitting in react

--- Web/Frontend/React/clean/src/App.js
+++ Web/Frontend/React/clean/src/App.js
@@ -1,6 +1,6 @@
 import { useState, StrictMode, lazy, Suspense } from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 import ThemeContext from "./ThemeContext";
 
 const Details = lazy(() => import("./Details"));
@@ -9,26 +9,28 @@ const SearchParams = lazy(() => import("./SearchParams"));
 const App = () => {
   const theme = useState("darkblue");
   return (
-    <ThemeContext.Provider value={theme}>
-      <div>
-        <h2>This h2 will not go away</h2>
-        <Suspense fallback={<h2>is loading</h2>}>
-          <Router>
-            <header>
-              <Link to="/">Adopt Me!</Link>
-            </header>
-            <Switch>
-              <Route path="/details/:id">
-                <Details />
-              </Route>
-              <Route path="/">
-                <SearchParams />
-              </Route>
-            </Switch>
-          </Router>
-        </Suspense>
-      </div>
-    </ThemeContext.Provider>
+    <StrictMode>
+      <ThemeContext.Provider value={theme}>
+        <div>
+          <h2>This h2 will not go away</h2>
+          <Suspense fallback={<h2>is loading</h2>}>
+            <Router>
+              <header>
+                <Link to="/">Adopt Me!</Link>
+              </header>
+              <Switch>
+                <Route path="/details/:id">
+                  <Details />
+                </Route>
+                <Route path="/">
+                  <SearchParams />
+                </Route>
+              </Switch>
+            </Router>
+          </Suspense>
+        </div>
+      </ThemeContext.Provider>
+    </StrictMode> 
   );
 };
 
